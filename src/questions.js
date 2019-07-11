e59293b57b0ef40ba08e3a3d82f5bb6b126801ea
Dan Shima <danning.cc@gmail.com>
2019-07-11 14:39:09 +0200

    added quiz and questions

---
+++ src/questions.js
@@ -0,0 +1,65 @@
+export const trainingQuestions = [
+    {
+      id: 'q1',
+      question: 'Do you often see the best in things?',
+      options: [
+        { id: 'q1a', label: 'Always', value: 1.0 },
+        { id: 'q1b', label: 'Usually, yeah', value: 0.7 },
+        { id: 'q1c', label: 'Not really', value: 0.2 },
+        { id: 'q1d', label: 'Never!', value: 0.0 },
+      ],
+    },
+    {
+      id: 'q2',
+      question: 'Do you feel like most things are your fault?',
+      options: [
+        { id: 'q2a', label: 'Everything I touch dies', value: 0.0 },
+        { id: 'q2b', label: 'Yes', value: 0.1 },
+        { id: 'q2c', label: 'Sometimes', value: 0.4 },
+        { id: 'q2d', label: 'Rarely', value: 0.8 },
+      ],
+    },
+    {
+      id: 'q3',
+      question: 'You look wonderful today.',
+      options: [
+        { id: 'q3a', label: 'Yah ik', value: 0.9 },
+        { id: 'q3b', label: 'Thanks!', value: 0.7 },
+        { id: 'q3c', label: 'I hate compliments', value: 0.1 },
+        { id: 'q3d', label: 'Ok', value: 0.5 },
+      ],
+    },
+    {
+      id: 'q4',
+      question: 'How do you feel about perfection?',
+      options: [
+        { id: 'q4a', label: 'Meh', value: 0.5 },
+        { id: 'q4b', label: 'Perfection is important to me', value: 0.3 },
+        { id: 'q4c', label: 'Everything I do must be perfect', value: 0.2 },
+        { id: 'q4d', label: "I don't mind as long as everyone's happy!", value: 1.0 },
+      ],
+    },
+    {
+      id: 'q5',
+      question: 'Imagine you have a absolutely delicious lasagna. An obscured wet floor sign causes you to slip and drop your lasagne on the floor.',
+      options: [
+        { id: 'q5a', label: 'I deserved that.', value: 0.0 },
+        { id: 'q5b', label: "These things happen. I'll get another", value: 1.0 },
+        { id: 'q5c', label: 'Top 5 worst days of my life', value: 0.3 },
+        { id: 'q5d', label: 'Rest in Pepperoni, lasagna.', value: 0.5 },
+      ],
+    },
+  ];
+  
+  export const validationQuestions = [
+    {
+      id: 'vq1',
+      question: 'How full is the proverbial glass?',
+      options: [
+        { id: 'vq1a', label: 'As empty as MY SOUL', value: 0.0 },
+        { id: 'vq1b', label: 'Definitely half empty', value: 0.3 },
+        { id: 'vq1c', label: 'It\'s half full', value: 0.6 },
+        { id: 'vq1d', label: 'Full to the brim', value: 1.0 },
+      ],
+    },
+  ];
