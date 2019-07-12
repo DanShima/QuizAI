b3d1d051e3c9296af1405b4b152011e059d6e31b
Dan Shima <danning.cc@gmail.com>
2019-07-12 23:10:21 +0200

    add another validation question

--- src/questions.js
+++ src/questions.js
@@ -1,12 +1,13 @@
+//it's important to vary the order of the values, which are used for the neural network
 export const trainingQuestions = [
     {
       id: 'q1',
       question: 'Do you often see the best in things?',
       options: [
+        { id: 'q1d', label: 'Never!', value: 0.0 },
         { id: 'q1a', label: 'Always', value: 1.0 },
         { id: 'q1b', label: 'Usually, yeah', value: 0.7 },
         { id: 'q1c', label: 'Not really', value: 0.2 },
-        { id: 'q1d', label: 'Never!', value: 0.0 },
       ],
     },
     {
@@ -15,15 +16,15 @@ export const trainingQuestions = [
       options: [
         { id: 'q2a', label: 'Everything I touch dies', value: 0.0 },
         { id: 'q2b', label: 'Yes', value: 0.1 },
-        { id: 'q2c', label: 'Sometimes', value: 0.4 },
         { id: 'q2d', label: 'Rarely', value: 0.8 },
+        { id: 'q2c', label: 'Sometimes', value: 0.4 },
       ],
     },
     {
       id: 'q3',
       question: 'You look wonderful today.',
       options: [
-        { id: 'q3a', label: 'Yah ik', value: 0.9 },
+        { id: 'q3a', label: 'Yah that makes my day!', value: 0.9 },
         { id: 'q3b', label: 'Thanks!', value: 0.7 },
         { id: 'q3c', label: 'I hate compliments', value: 0.1 },
         { id: 'q3d', label: 'Ok', value: 0.5 },
@@ -41,12 +42,12 @@ export const trainingQuestions = [
     },
     {
       id: 'q5',
-      question: 'Imagine you have a absolutely delicious lasagna. An obscured wet floor sign causes you to slip and drop your lasagne on the floor.',
+      question: 'Imagine you have a absolutely delicious pizza. An obscured wet floor causes you to slip and drop your pizza on the floor.',
       options: [
         { id: 'q5a', label: 'I deserved that.', value: 0.0 },
         { id: 'q5b', label: "These things happen. I'll get another", value: 1.0 },
         { id: 'q5c', label: 'Top 5 worst days of my life', value: 0.3 },
-        { id: 'q5d', label: 'Rest in Pepperoni, lasagna.', value: 0.5 },
+        { id: 'q5d', label: 'R.i.P, pizza.', value: 0.5 },
       ],
     },
   ];
@@ -61,5 +62,15 @@ export const trainingQuestions = [
         { id: 'vq1c', label: 'It\'s half full', value: 0.6 },
         { id: 'vq1d', label: 'Full to the brim', value: 1.0 },
       ],
+    },
+    {
+        id: 'vq2',
+        question: 'Do you have trust and faith in people?',
+        options: [
+          { id: 'vq2a', label: 'They are only out to destroy me', value: 0.0 },
+          { id: 'vq2b', label: 'Rarely', value: 0.2 },
+          { id: 'vq2c', label: 'Most of the times', value: 0.8 },
+          { id: 'vq2d', label: 'Always', value: 1.0 },
+        ],
     },
   ];
