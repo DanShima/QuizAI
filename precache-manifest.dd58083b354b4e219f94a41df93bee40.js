355ad6ecf37c26bec6f056b65022f1410c163e03
Dan Shima <danning.cc@gmail.com>
2019-07-13 11:35:24 +0200

    Updates

---
+++ precache-manifest.dd58083b354b4e219f94a41df93bee40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99a8a0bdbe69eedf7ca62f927c842e9",
+    "url": "/quizai/index.html"
+  },
+  {
+    "revision": "6c354a7027982a58ebe5",
+    "url": "/quizai/static/css/main.5d60cfc2.chunk.css"
+  },
+  {
+    "revision": "638b4ce6faaa0b4d9b8f",
+    "url": "/quizai/static/js/2.5ae6aabf.chunk.js"
+  },
+  {
+    "revision": "6c354a7027982a58ebe5",
+    "url": "/quizai/static/js/main.900e882d.chunk.js"
+  },
+  {
+    "revision": "5cfe3835dab15327e727",
+    "url": "/quizai/static/js/runtime~main.b6b65582.js"
+  }
+]);
